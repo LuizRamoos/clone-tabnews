807f07970d4f3924b09c320cd7f7b170bb358341
LuizRamoos <139016106+LuizRamoos@users.noreply.github.com>
2024-05-02 13:57:29 +0000

    removendo funcoes teste

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Mãe, vamos fazer o pavê ou empadão??? 😂</h1>;
 }
 
-function teste() {
-  console.log("Teste");
-}
-
-function teste2() {
-  console.log("identação errada");
-}
-
 export default Home;
